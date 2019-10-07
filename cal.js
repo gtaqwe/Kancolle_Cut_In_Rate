9d3c8a75a5f8c00c01500b877ad61e3a32ebfbdd
ZEED <gtaqwe@gmail.com>
2019-10-07 16:52:57 +0900

    Add files via upload

--- cal.js
+++ cal.js
@@ -15,12 +15,15 @@ function get_number(id, max) {
   return num;
 }
 
+function cutin_cal(main_cal, mode) {
+	return  main_cal / mode * 100
+}
 
 
 function update() {
 	var lv				= get_number("input_lv", 9999)
 	var luck			= get_number("input_luck", 9999)
-	var mode			= get_number("mode", 9999)
+	//var mode			= get_number("mode", 9999)
 	var flagship		= get_number("flagship", 9999)
 	var medium			= get_number("medium", 9999)
 	var searchlight		= get_number("searchlight", 9999)
@@ -40,8 +43,11 @@ function update() {
 	}
 	friend_cal = flagship + medium + searchlight + flare + lookout
 	enemy_cal = e_searchlight + e_flare
-	main_cal = Math.trunc(main_cal + friend_cal - enemy_cal) / mode * 100
-	$("p.result").text(main_cal.toFixed(2)+"%");
+	main_cal = Math.trunc(main_cal + friend_cal - enemy_cal)
+	$("#result_1").text("포뢰 : "+cutin_cal(main_cal, 115).toFixed(2)+"%");
+	$("#result_2").text("어뢰 : "+cutin_cal(main_cal, 122).toFixed(2)+"%");
+	$("#result_3").text("주부 : "+cutin_cal(main_cal, 130).toFixed(2)+"%");
+	$("#result_4").text("주포 : "+cutin_cal(main_cal, 140).toFixed(2)+"%");
 	
 }
 
